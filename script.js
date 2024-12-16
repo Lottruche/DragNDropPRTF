03360b2a97102c66c3e9af2016283f6f0a05327e
Lottruche <matthias.pgd95@gmail.com>
2024-12-16 16:02:38 +0100

    mise en place élément

---
+++ script.js
@@ -0,0 +1,17 @@
+var draggies = [];
+
+var draggableElems = document.querySelectorAll('.draggable');
+for (var i = 0; i < draggableElems.length; i++) {
+    var draggableElem = draggableElems[i];
+    var draggie = new Draggabilly(draggableElem, {});
+    draggies.push(draggie);
+}
+
+var draggableElems = document.querySelectorAll('.lampe');
+for (var i = 0; i < draggableElems.length; i++) {
+    var draggableElem = draggableElems[i];
+    var draggie = new Draggabilly(draggableElem, {
+                containment: '.lampeBox',
+    });
+    draggies.push(draggie);
+}
